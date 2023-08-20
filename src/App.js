d9f083796681914c839f84a91b720ec39f42f747
shu2002k <shubhkuttiya@gmail.com>
2023-08-20 10:06:44 -0700

    secon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './Dashboard';
 
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Example from './Example';
+import { ToastContainer } from 'react-toastify';
+import AddCourse from './course/AddCourse';
+// import Demo from './Demo';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <Router>
+
+        <Routes>
+      
+            {/* <Route exact path='/' Component={Demo} /> */}
+            <Route exact path='/:id' Component={Dashboard}/>
+            <Route exact path='/addCourse' Component={AddCourse}/>
+
+           
+        </Routes>
+       </Router>
+    </>
   );
 }
 
---
+++ src/LineChartComponent.jsx
@@ -0,0 +1,35 @@
+import { DataUsageSharp } from '@material-ui/icons';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+
+function LineChartComponent() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        loadUser();
+    },[]);
+
+    //  for the gett All Products
+    const loadUser = async () => {
+        const result = await axios.get("http://localhost:9096/getPay");
+        setData(result.data);
+    }
+
+    return (
+        <>
+            <ResponsiveContainer width="100%" height="100%">
+                <LineChart data={data}>
+                    <XAxis dataKey="key" stroke="black"/>
+                    <YAxis stroke="black"/>
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="amount" stroke="red" activeDot={{r:8}} />
+                </LineChart>
+            </ResponsiveContainer>
+
+        </>
+    )
+}
+
+export default LineChartComponent
